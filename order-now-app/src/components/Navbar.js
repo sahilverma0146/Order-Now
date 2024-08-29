1483078bdcf475710211945ce0b1bfa50a85fb28
Sahil <sahilverma22146@gmail.com>
2024-08-29 21:32:29 +0530

    navbar feature

---
+++ order-now-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../Style/Navbar.css';
+function Navbar() {
+  return (
+    <>
+      <div className="Navbar">
+        <div>
+          <h>ORDER_NOW</h>
+        </div>
+        <input placeholder="Search For Products..." type="text"></input>
+        <button><i class="fa-sharp fa-solid fa-cart-plus"></i> <span>5</span> <i class="fa-solid fa-caret-down"></i></button>
+      </div>
+    </>
+  );
+}
+export default Navbar;
